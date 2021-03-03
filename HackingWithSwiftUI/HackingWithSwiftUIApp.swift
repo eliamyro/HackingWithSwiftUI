b42656d61ee91dba5eda054008acc6b7a26701ae
eliamyro <elias.myronidis@hotmail.com>
2021-03-03 20:53:16 +0200

    Initial Commit

---
+++ HackingWithSwiftUI/HackingWithSwiftUIApp.swift
@@ -0,0 +1,15 @@
+//
+// Copyright © 2021 ___ORGANIZATIONNAME___
+// All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
